5527ff535fd617110b37c25ffd0f6026c7811538
valqua2007 <quanduongforworks2007@gmail.com>
2021-11-28 13:45:08 +0700

    react router v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import Button from "./components/Button";
-import GlobalStyles from "./components/GlobalStyles";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import { Route, Routes, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <GlobalStyles>
-        <Button primary />
-      </GlobalStyles>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {/* <Route path = "/" element = {Home}/> */}
+        {/* Cái hôm kia đang được gọi là react component nhưng ở bạn v6 này chúng ta cần react-element nên phải
+        viết như này */}
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Home page</h1>
+}
+console.log("Home page ");
+export default Home;
